ef976abd007fd5ad7169d845fd738936f2480efb
kailasdayanandan <kailasdayanandan@gmail.com>
2020-09-16 21:45:45 +0530

    Download Grid Corpus dataset for Lipreading
    Download Grid Corpus data set for grid_corpus folder

---
+++ download_dataset.sh
@@ -0,0 +1,141 @@
+mkdir grid_corpus
+mkdir grid_corpus/video
+mkdir grid_corpus/align
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s1/video/s1.mpg_vcd.zip -O ./grid_corpus/video/s1.mpg_vcd.zip
+unzip ./grid_corpus/video/s1.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s2/video/s2.mpg_vcd.zip -O ./grid_corpus/video/s2.mpg_vcd.zip
+unzip ./grid_corpus/video/s2.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s3/video/s3.mpg_vcd.zip -O ./grid_corpus/video/s3.mpg_vcd.zip
+unzip ./grid_corpus/video/s3.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s4/video/s4.mpg_vcd.zip -O ./grid_corpus/video/s4.mpg_vcd.zip
+unzip ./grid_corpus/video/s4.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s5/video/s5.mpg_vcd.zip -O ./grid_corpus/video/s5.mpg_vcd.zip
+unzip ./grid_corpus/video/s5.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s6/video/s6.mpg_vcd.zip -O ./grid_corpus/video/s6.mpg_vcd.zip
+unzip ./grid_corpus/video/s6.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s7/video/s7.mpg_vcd.zip -O ./grid_corpus/video/s7.mpg_vcd.zip
+unzip ./grid_corpus/video/s7.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s8/video/s8.mpg_vcd.zip -O ./grid_corpus/video/s8.mpg_vcd.zip
+unzip ./grid_corpus/video/s8.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s9/video/s9.mpg_vcd.zip -O ./grid_corpus/video/s9.mpg_vcd.zip
+unzip ./grid_corpus/video/s9.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s10/video/s10.mpg_vcd.zip -O ./grid_corpus/video/s10.mpg_vcd.zip
+unzip ./grid_corpus/video/s10.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s11/video/s11.mpg_vcd.zip -O ./grid_corpus/video/s11.mpg_vcd.zip
+unzip ./grid_corpus/video/s11.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s12/video/s12.mpg_vcd.zip -O ./grid_corpus/video/s12.mpg_vcd.zip
+unzip ./grid_corpus/video/s12.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s13/video/s13.mpg_vcd.zip -O ./grid_corpus/video/s13.mpg_vcd.zip
+unzip ./grid_corpus/video/s13.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s14/video/s14.mpg_vcd.zip -O ./grid_corpus/video/s14.mpg_vcd.zip
+unzip ./grid_corpus/video/s14.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s15/video/s15.mpg_vcd.zip -O ./grid_corpus/video/s15.mpg_vcd.zip
+unzip ./grid_corpus/video/s15.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s16/video/s16.mpg_vcd.zip -O ./grid_corpus/video/s16.mpg_vcd.zip
+unzip ./grid_corpus/video/s16.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s17/video/s17.mpg_vcd.zip -O ./grid_corpus/video/s17.mpg_vcd.zip
+unzip ./grid_corpus/video/s17.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s18/video/s18.mpg_vcd.zip -O ./grid_corpus/video/s18.mpg_vcd.zip
+unzip ./grid_corpus/video/s18.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s19/video/s19.mpg_vcd.zip -O ./grid_corpus/video/s19.mpg_vcd.zip
+unzip ./grid_corpus/video/s19.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s20/video/s20.mpg_vcd.zip -O ./grid_corpus/video/s20.mpg_vcd.zip
+unzip ./grid_corpus/video/s20.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s21/video/s21.mpg_vcd.zip -O ./grid_corpus/video/s21.mpg_vcd.zip
+unzip ./grid_corpus/video/s21.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s22/video/s22.mpg_vcd.zip -O ./grid_corpus/video/s22.mpg_vcd.zip
+unzip ./grid_corpus/video/s22.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s23/video/s23.mpg_vcd.zip -O ./grid_corpus/video/s23.mpg_vcd.zip
+unzip ./grid_corpus/video/s23.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s24/video/s24.mpg_vcd.zip -O ./grid_corpus/video/s24.mpg_vcd.zip
+unzip ./grid_corpus/video/s24.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s25/video/s25.mpg_vcd.zip -O ./grid_corpus/video/s25.mpg_vcd.zip
+unzip ./grid_corpus/video/s25.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s26/video/s26.mpg_vcd.zip -O ./grid_corpus/video/s26.mpg_vcd.zip
+unzip ./grid_corpus/video/s26.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s27/video/s27.mpg_vcd.zip -O ./grid_corpus/video/s27.mpg_vcd.zip
+unzip ./grid_corpus/video/s27.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s28/video/s28.mpg_vcd.zip -O ./grid_corpus/video/s28.mpg_vcd.zip
+unzip ./grid_corpus/video/s28.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s29/video/s29.mpg_vcd.zip -O ./grid_corpus/video/s29.mpg_vcd.zip
+unzip ./grid_corpus/video/s29.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s30/video/s30.mpg_vcd.zip -O ./grid_corpus/video/s30.mpg_vcd.zip
+unzip ./grid_corpus/video/s30.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s31/video/s31.mpg_vcd.zip -O ./grid_corpus/video/s31.mpg_vcd.zip
+unzip ./grid_corpus/video/s31.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s32/video/s32.mpg_vcd.zip -O ./grid_corpus/video/s32.mpg_vcd.zip
+unzip ./grid_corpus/video/s32.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s33/video/s33.mpg_vcd.zip -O ./grid_corpus/video/s33.mpg_vcd.zip
+unzip ./grid_corpus/video/s33.mpg_vcd.zip -d grid_corpus/video/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s34/video/s34.mpg_vcd.zip -O ./grid_corpus/video/s34.mpg_vcd.zip
+unzip ./grid_corpus/video/s34.mpg_vcd.zip -d grid_corpus/video/
+rm -rf ./grid_corpus/video/*.zip
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s1/align/s1.tar -O ./grid_corpus/align/s1.tar
+tar -xf ./grid_corpus/align/s1.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s2/align/s2.tar -O ./grid_corpus/align/s2.tar
+tar -xf ./grid_corpus/align/s2.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s3/align/s3.tar -O ./grid_corpus/align/s3.tar
+tar -xf ./grid_corpus/align/s3.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s4/align/s4.tar -O ./grid_corpus/align/s4.tar
+tar -xf ./grid_corpus/align/s4.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s5/align/s5.tar -O ./grid_corpus/align/s5.tar
+tar -xf ./grid_corpus/align/s5.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s6/align/s6.tar -O ./grid_corpus/align/s6.tar
+tar -xf ./grid_corpus/align/s6.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s7/align/s7.tar -O ./grid_corpus/align/s7.tar
+tar -xf ./grid_corpus/align/s7.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s8/align/s8.tar -O ./grid_corpus/align/s8.tar
+tar -xf ./grid_corpus/align/s8.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s9/align/s9.tar -O ./grid_corpus/align/s9.tar
+tar -xf ./grid_corpus/align/s9.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s10/align/s10.tar -O ./grid_corpus/align/s10.tar
+tar -xf ./grid_corpus/align/s10.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s11/align/s11.tar -O ./grid_corpus/align/s11.tar
+tar -xf ./grid_corpus/align/s11.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s12/align/s12.tar -O ./grid_corpus/align/s12.tar
+tar -xf ./grid_corpus/align/s12.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s13/align/s13.tar -O ./grid_corpus/align/s13.tar
+tar -xf ./grid_corpus/align/s13.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s14/align/s14.tar -O ./grid_corpus/align/s14.tar
+tar -xf ./grid_corpus/align/s14.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s15/align/s15.tar -O ./grid_corpus/align/s15.tar
+tar -xf ./grid_corpus/align/s15.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s16/align/s16.tar -O ./grid_corpus/align/s16.tar
+tar -xf ./grid_corpus/align/s16.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s17/align/s17.tar -O ./grid_corpus/align/s17.tar
+tar -xf ./grid_corpus/align/s17.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s18/align/s18.tar -O ./grid_corpus/align/s18.tar
+tar -xf ./grid_corpus/align/s18.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s19/align/s19.tar -O ./grid_corpus/align/s19.tar
+tar -xf ./grid_corpus/align/s19.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s20/align/s20.tar -O ./grid_corpus/align/s20.tar
+tar -xf ./grid_corpus/align/s20.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s21/align/s21.tar -O ./grid_corpus/align/s21.tar
+tar -xf ./grid_corpus/align/s21.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s22/align/s22.tar -O ./grid_corpus/align/s22.tar
+tar -xf ./grid_corpus/align/s22.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s23/align/s23.tar -O ./grid_corpus/align/s23.tar
+tar -xf ./grid_corpus/align/s23.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s24/align/s24.tar -O ./grid_corpus/align/s24.tar
+tar -xf ./grid_corpus/align/s24.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s25/align/s25.tar -O ./grid_corpus/align/s25.tar
+tar -xf ./grid_corpus/align/s25.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s26/align/s26.tar -O ./grid_corpus/align/s26.tar
+tar -xf ./grid_corpus/align/s26.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s27/align/s27.tar -O ./grid_corpus/align/s27.tar
+tar -xf ./grid_corpus/align/s27.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s28/align/s28.tar -O ./grid_corpus/align/s28.tar
+tar -xf ./grid_corpus/align/s28.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s29/align/s29.tar -O ./grid_corpus/align/s29.tar
+tar -xf ./grid_corpus/align/s29.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s30/align/s30.tar -O ./grid_corpus/align/s30.tar
+tar -xf ./grid_corpus/align/s30.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s31/align/s31.tar -O ./grid_corpus/align/s31.tar
+tar -xf ./grid_corpus/align/s31.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s32/align/s32.tar -O ./grid_corpus/align/s32.tar
+tar -xf ./grid_corpus/align/s32.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s33/align/s33.tar -O ./grid_corpus/align/s33.tar
+tar -xf ./grid_corpus/align/s33.tar -C ./grid_corpus/align/
+wget http://spandh.dcs.shef.ac.uk/gridcorpus/s34/align/s34.tar -O ./grid_corpus/align/s34.tar
+tar -xf ./grid_corpus/align/s34.tar -C ./grid_corpus/align/
+rm -rf ./grid_corpus/align/*.tar
